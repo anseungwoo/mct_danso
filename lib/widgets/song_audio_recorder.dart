44450d46621ec9b4f0ae06c231dd7be9d8d1305d
anseungwoo <asw0899@naver.com>
2021-10-20 17:27:31 +0900

    녹음기능: ios테스트 파일럿으로 실행가능한지 확인함 update: song_audio_recorder.dart 파일을 ios에서 불가한 코드에서 ->ios에서 가능한 api로 작성했음 추가 설명은 아래에 pubspec.yaml 에추가한 api flutter_audio_recorder: ^0.5.5 audioplayers: ^0.17.4
    정간보 기존 4/12에서 4/6으로 변경하여 적용함
    play_and_test_controller.drat 파일에 한글 한문 버튼 바꾸는 로직작성
    
    audioplayers: ^0.17.4 추가로인한 코틀린 버전변경
    android/src  build.gradle 에
     configurations.all {
                resolutionStrategy { force 'androidx.core:core-ktx:1.6.0' }
            }
    구문을 추가한 이것 또한 audioplayers에 의한 추가
    ext.kotlin_version = '1.3.50'-> ext.kotlin_version = '1.4.32'
    <description>
    _changeState 녹화 시작 정지 상태를 바꿔주고 실행 하는 함
    setState 녹화상태에따라서 버튼의 글을 바꿔어
    _init 저장 경로를 설정하고 initialized해줌
    _prepare 퍼미션 허용에 따라서 init을실행하고 상태를 저장해줌
    _startRecording 녹음을 시작하고 시간을 측정하고  녹음 start상태로 바꿈
    _stopRecording 녹음을 멈추고 시간측정을 멈춤 녹음 stop상태로 바꿈 2번 버튼상태로 이동
    _buttonTextState 녹음 상태에 따라 녹화시작 멈춤 으로 바꾸는 로직
    
    <issue-number>

--- lib/widgets/song_audio_recorder.dart
+++ lib/widgets/song_audio_recorder.dart
@@ -1,86 +1,167 @@
-import 'dart:io' as io;
-import 'package:audio_recorder/audio_recorder.dart';
-import 'package:file/file.dart';
-import 'package:file/local.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
+import 'dart:io' as io;
+import 'dart:async';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_audio_recorder/flutter_audio_recorder.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:project_danso/controllers/controllers.dart';
 
 class SongAudioRecorder extends StatefulWidget {
-  final LocalFileSystem localFileSystem;
   PlayAndTestController controller;
-  SongAudioRecorder({localFileSystem, this.controller})
-      : this.localFileSystem = localFileSystem ?? LocalFileSystem();
-
+  SongAudioRecorder({Key key, this.controller, PlayAndTestController contoller})
+      : super(key: key);
   @override
   State<StatefulWidget> createState() => new SongAudioRecorderState();
 }
 
 class SongAudioRecorderState extends State<SongAudioRecorder> {
-  Recording _recording = new Recording();
-  bool _isRecording = false;
+  FlutterAudioRecorder _recorder;
+  Recording _recording;
+  Timer _time;
+  Widget _buttonText = Text("녹화오류");
+  String _alert;
 
-  TextEditingController _controller = new TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      _prepare();
+    });
+  }
+
+  void _changeState() async {
+    switch (_recording.status) {
+      case RecordingStatus.Initialized:
+        {
+          await _startRecording();
+          break;
+        }
+      case RecordingStatus.Recording:
+        {
+          await _stopRecording();
+          break;
+        }
+      case RecordingStatus.Stopped:
+        {
+          await _prepare();
+          break;
+        }
+
+      default:
+        break;
+    }
+
+    // 刷新按钮
+    setState(() {
+      _buttonText = _buttonTextState(_recording.status);
+    });
+  }
+
+  Future _init() async {
+    String customPath = '/flutter_audio_recorder_';
+    io.Directory appDocDirectory;
+    if (io.Platform.isIOS) {
+      appDocDirectory = await getApplicationDocumentsDirectory();
+    } else {
+      appDocDirectory = await getExternalStorageDirectory();
+    }
+
+    // can add extension like ".mp4" ".wav" ".m4a" ".aac"
+    customPath = appDocDirectory.path +
+        customPath +
+        DateTime.now().millisecondsSinceEpoch.toString();
+
+    // .wav <---> AudioFormat.WAV
+    // .mp4 .m4a .aac <---> AudioFormat.AAC
+    // AudioFormat is optional, if given value, will overwrite path extension when there is conflicts.
+
+    _recorder = FlutterAudioRecorder(customPath,
+        audioFormat: AudioFormat.WAV, sampleRate: 22050);
+    await _recorder.initialized;
+  }
+
+  Future _prepare() async {
+    var hasPermission = await FlutterAudioRecorder.hasPermissions;
+    if (hasPermission) {
+      await _init();
+      var result = await _recorder.current();
+      setState(() {
+        _recording = result;
+        _buttonText = _buttonTextState(_recording.status);
+        _alert = "";
+      });
+    } else {
+      setState(() {
+        _alert = "Permission Required.";
+      });
+    }
+  }
+
+  Future _startRecording() async {
+    await _recorder.start();
+    var current = await _recorder.current();
+    setState(() {
+      _recording = current;
+    });
+
+    _time = Timer.periodic(Duration(milliseconds: 10), (Timer t) async {
+      var current = await _recorder.current();
+      setState(() {
+        _recording = current;
+        _time = t;
+      });
+    });
+  }
+
+  Future _stopRecording() async {
+    var result = await _recorder.stop();
+    _time.cancel();
+
+    setState(() {
+      _recording = result;
+    });
+    widget.controller.stateCountUp(2);
+  }
+
+  void _play() {
+    AudioPlayer player = AudioPlayer();
+    player.play(_recording.path, isLocal: true);
+  }
+
+  Widget _buttonTextState(RecordingStatus status) {
+    switch (status) {
+      case RecordingStatus.Initialized:
+        {
+          return Text("녹화시작");
+        }
+      case RecordingStatus.Recording:
+        {
+          return Text("녹화멈춤");
+        }
+      case RecordingStatus.Stopped:
+        {
+          return Text("녹화정지");
+        }
+      default:
+        return Icon(Icons.do_not_disturb_on);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Row(children: <Widget>[
-      FlatButton(
-        color: Colors.blue,
-        onPressed: _isRecording ? _stop : _start,
-        child: _isRecording ? Text('녹음중지') : Text("녹음 시작"),
+      ElevatedButton(
+        onPressed: _changeState,
+        child: _buttonText,
       ),
       SizedBox(width: 5.w),
-      FlatButton(
+      ElevatedButton(
         onPressed: () {},
         child: Text("반주만"),
-        color: Colors.blue,
       ),
     ]);
-  }
 
-  _start() async {
-    try {
-      if (await AudioRecorder.hasPermissions) {
-        if (_controller.text != null && _controller.text != "") {
-          String path = _controller.text;
-          if (!_controller.text.contains('/')) {
-            io.Directory appDocDirectory =
-                await getApplicationDocumentsDirectory();
-            path = appDocDirectory.path + '/' + _controller.text;
-          }
-          print("Start recording: $path");
-          await AudioRecorder.start(
-              path: path, audioOutputFormat: AudioOutputFormat.AAC);
-        } else {
-          await AudioRecorder.start();
-        }
-        bool isRecording = await AudioRecorder.isRecording;
-        setState(() {
-          _recording = new Recording(duration: new Duration(), path: "");
-          _isRecording = isRecording;
-        });
-      } else {
-        Scaffold.of(context).showSnackBar(
-            new SnackBar(content: new Text("You must accept permissions")));
-      }
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  _stop() async {
-    var recording = await AudioRecorder.stop();
-    print("Stop recording: ${recording.path}");
-    bool isRecording = await AudioRecorder.isRecording;
-    File file = widget.localFileSystem.file(recording.path);
-    print("  File length: ${await file.length()}");
-    setState(() {
-      _recording = recording;
-      _isRecording = isRecording;
-    });
-    _controller.text = recording.path;
-    widget.controller.stateCountUp(2);
+    // This trailing comma makes auto-formatting nicer for build methods.
   }
 }
